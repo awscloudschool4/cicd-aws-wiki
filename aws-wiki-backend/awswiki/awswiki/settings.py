be5c6cbe31b0415d0bb9c13e59cdd9d98fbab027
Alice <jaeyeon0921@gmail.com>
2024-05-13 15:10:39 +0900

    add: django secret key

--- aws-wiki-backend/awswiki/awswiki/settings.py
+++ aws-wiki-backend/awswiki/awswiki/settings.py
@@ -26,11 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = my_settings.SECRET_KEY
+SECRET_KEY = 'django-insecure-k1_u6012qar0m!0!c=ibszh7*(rx897%6f59wl9&8guvr%mszr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
@@ -56,10 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'corsheaders',
-
     'job',
     'note',
     'photo'
