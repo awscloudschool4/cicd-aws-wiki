598678b97b88c3cf11a5bb6e2b8b73257910c5c9
Alice <jaeyeon0921@gmail.com>
2024-05-17 07:42:21 +0900

    add: pattern for next image

--- aws-wiki-frontend/next.config.mjs
+++ aws-wiki-frontend/next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
